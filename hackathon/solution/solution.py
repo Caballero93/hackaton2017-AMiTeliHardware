7b9146b8bb03d013bc6f1460cad9b9b8315fdcc1
miloshunter <milostravian2@gmail.com>
2017-11-04 20:59:55 +0100

    Ispravljeni neki bagovi. Proba6. Na 1439 trosi 400, a do kraja 2195.

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -11,6 +11,9 @@ def worker(msg: DataMessage) -> ResultsMessage:
     # Details about DataMessage and ResultsMessage objects can be found in /utils/utils.py
     # Dummy result is returned in every cycle here
     p_bat = 0.0
+    panel = PVMode.ON
+    load1 = True
+    load2 = True
 
     if msg.buying_price == 8 and msg.solar_production < 1:
         load3 = False
@@ -18,34 +21,31 @@ def worker(msg: DataMessage) -> ResultsMessage:
         load3 = True
 
     if msg.grid_status:
-        load1 = True
-        load2 = True
+        # if msg.buying_price == 8 and msg.solar_production < 1 and msg.bessSOC < 0.3:
+        #     load2 = False
+
+        bla2 = msg.current_load * 0.5 if load2 else 0
 
         if msg.buying_price == 8:
-            if msg.bessSOC > 0.192:
-                if msg.solar_production > msg.current_load:
-                    p_bat = msg.current_load - msg.solar_production
+            # if msg.current_load - bla2 > 8.5:
+            #     load2 = False
+
+            if msg.selling_price == 0:
+                temp = msg.solar_production - msg.current_load + bla2
+                if temp > 0:
+                    p_bat = -temp
                 else:
-                    p_bat = 2
+                    p_bat = 5 * msg.mainGridPower / 8
             else:
-                p_bat = -1.0
-            if msg.current_load > 8.0:
-                load2 = False
-            else:
-                load2 = True
+                p_bat = 5 * msg.mainGridPower / 8
         else:
-            p_bat = -2.0
+            p_bat = -6.0
             if msg.solar_production > msg.current_load:
-                p_bat = msg.current_load - msg.solar_production
+                p_bat -= msg.current_load - msg.solar_production
     else:
         load1 = True
         load2 = True
         load3 = True
-        # if msg.solar_production > 2:
-        #     load2 = True
-        # if msg.bessSOC > 0.6:
-        #     load2 = True
-        # p_bat = 6
         temp = msg.solar_production + 6 - msg.current_load
         if temp < 0:
             if temp > -0.3*msg.current_load:
@@ -53,21 +53,29 @@ def worker(msg: DataMessage) -> ResultsMessage:
             else:
                 load3, load2 = False, False
 
-        if msg.current_load < 5.6:
+        if msg.bessSOC < 0.56:
             load3 = False
-        elif msg.current_load < 1.7:
+        if msg.bessSOC < 0.17:
             load3, load2 = False, False
 
+        bla2 = msg.current_load * 0.5 if load2 else 0
+        bla3 = msg.current_load * 0.3 if load3 else 0
 
-    if msg.selling_price == 0 and msg.grid_status:
-        p_bat = -2
+        if (msg.solar_production > msg.current_load*0.2 + bla2 + bla3) and (msg.bessSOC > 0.99):
+            panel = PVMode.OFF
+
+    if msg.bessSOC < 0.192 and p_bat > 0:
+        p_bat = 0.0
+
+    # if msg.selling_price == 0 and msg.grid_status:
+    #     p_bat = -2
 
     return ResultsMessage(data_msg=msg,
                           load_one=load1,
                           load_two=load2,
                           load_three=load3,
                           power_reference=float(p_bat),
-                          pv_mode=PVMode.ON)
+                          pv_mode=panel)
 
 
 def run(args) -> None:
